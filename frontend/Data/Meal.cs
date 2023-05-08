7108baf6733e4849609061f468b4b4c51d38ba8f
studwoehmo2375 <monika.woehrl@stud.fh-rosenheim.de>
2023-05-08 11:05:28 +0200

    Added delete comment to Survey prompt

---
+++ frontend/Data/Meal.cs
@@ -0,0 +1,14 @@
+﻿namespace frontend.Data
+{
+    public class Meal
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+
+        public string? Instructions { get; set; }
+
+        public string? Imagelink { get; set; }
+
+        public string? Youtubelink { get; set; }
+    }
+}
