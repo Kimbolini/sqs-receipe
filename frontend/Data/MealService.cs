ae1b1f8ee856a463d8946f3e9b3940d734f3c3e4
studwoehmo2375 <monika.woehrl@stud.fh-rosenheim.de>
2023-05-27 10:28:51 +0200

    Added meal cards with static data

---
+++ frontend/Data/MealService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.Json.Serialization;
+
+namespace frontend.Data
+{
+    public class MealService : PageModel
+    {
+        public async Task<List<Meal>> GetMealsAsync(string search)
+        {
+            List<Meal> meals = new List<Meal>();
+            using var httpClient = new HttpClient();
+            using (HttpResponseMessage response = await httpClient.GetAsync("https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata"))
+            {
+                /*if (response.IsSuccessStatusCode)
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<List<Meal>>(apiResponse);
+                } else
+                {
+                    return meals;
+                }*/
+                return meals;
+            }
+       
+        }
+    }
+}
