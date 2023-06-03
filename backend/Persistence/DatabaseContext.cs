bb769755431738f15404e7580c689a4279ba28ee
studwoehmo2375 <monika.woehrl@stud.fh-rosenheim.de>
2023-06-03 16:41:11 +0200

    Fixed namesapce errors

--- backend/Persistence/DatabaseContext.cs
+++ backend/Persistence/DatabaseContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace backend.Persistence
+namespace backend.Models
 {
     /*
      * This class defines the database scheme.
--- backend/Startup.cs
+++ backend/Startup.cs
@@ -1,10 +1,23 @@
-﻿using backend.Persistence;
+﻿using backend.Models;
 using Microsoft.EntityFrameworkCore;
+using log4net;
 
 namespace backend
 {
     public class Startup
     {
+        /*
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            })
+            .ConfigureLogging(builder =>
+            {
+            builder.AddLog4Net();
+        });*/
+
         public IConfiguration Configuration { get; }
 
         public Startup(IConfiguration configuration)
