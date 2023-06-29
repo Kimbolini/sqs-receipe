b9630d0fae16791059e3ea61204256a1c510732c
studwoehmo2375 <monika.woehrl@stud.fh-rosenheim.de>
2023-06-29 16:57:08 +0200

    added js k6 script

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+import { check, sleep } from "k6";
+import http from "k6/http";
+
+export let options = {
+    stages: [
+        { duration: "20s", target: 10 }
+    ]
+};
+
+/* Main function */
+export default function() {
+    var r = http.get("http://test.loadimpact.com");
+    check(r, {
+        "status is 200": (r) => r.status === 200
+    });
+    sleep(1);
+}
